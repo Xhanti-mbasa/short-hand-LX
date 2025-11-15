e0a208592e7fdeb7827d909773ff0ce668c0e5ed
Jaydinx7 <jaydinsolomons7@gmail.com>
2025-11-15 11:15:37 +0200

    Implement command execution verification
    Added user confirmation before executing alias command.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 # #options
 # zsh = 1
@@ -7,15 +8,20 @@ import subprocess
 # TODO add more structure and features.
 def alias():
     shell_options = str(input(f"Enter your shell type:\n1.zsh\n2.bash\n:")).lower()
+    
     if shell_options == "1":
         shell_options = "./zshrc"
     elif shell_options == "2":
         shell_options = "./bashrc"
+        
     short_hand = input("Type out your short hand: ")
     command = input("Now the command you'll be replacing: ")
-    original_command = f"echo 'alias {short_hand}='{command}'' >> {shell_options}"
-    subprocess.run(original_command, shell=True)
-    print(original_command)
+    verification_input = input("Are you sure you want to execute this command? Y/n: ").lower().strip()
+    
+    if verification_input == "y":
+        original_command = f"echo 'alias {short_hand}='{command}'' >> {shell_options}"
+        subprocess.run(original_command, shell=True)
+        print(original_command)
 # TODO add execution safety.
 def simple_command():
     simple_command1 = input("Type out a command: ")
